c2f0327f2f58b12049b43bdeac340b4712adde87
nyxb <dennis.ollhoff@joypix.gg>
2024-06-28 18:41:02 +0200

    ✨ feat(utils/get-project-info.ts): add isTypeScriptProject to check for tsconfig.json in cwd
    ✨ feat(utils/get-project-info.ts): add preFlight function to ensure Tailwind CSS is configured

--- src/utils/get-project-info.ts
+++ src/utils/get-project-info.ts
@@ -144,3 +144,19 @@ export async function getTsConfigAliasPrefix(cwd: string) {
 
    return null
 }
+
+export async function isTypeScriptProject(cwd: string) {
+   // Check if cwd has a tsconfig.json file.
+   return pathExists(path.resolve(cwd, 'tsconfig.json'))
+}
+
+export async function preFlight(cwd: string) {
+   // We need Tailwind CSS to be configured.
+   const tailwindConfig = await fg.glob('tailwind.config.*', {
+      cwd,
+      deep: 3,
+      ignore: PROJECT_SHARED_IGNORE,
+   })
+
+   if (!tailwindConfig.length) {
+      throw new Error(
